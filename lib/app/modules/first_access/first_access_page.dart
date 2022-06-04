1ee46f653a75cf897e38d9f894ddf0df55b8ab26
Paulo Walraven <pmwc03@gmail.com>
2022-06-04 17:48:56 -0300

    create first access page

--- lib/app/modules/first_access/first_access_page.dart
+++ lib/app/modules/first_access/first_access_page.dart
@@ -1,4 +1,7 @@
 import 'package:darth_vaders_tabata/app/modules/first_access/first_access_controller.dart';
+import 'package:darth_vaders_tabata/core/consts/asset_consts.dart';
+import 'package:darth_vaders_tabata/core/theme/app_colors.dart';
+import 'package:darth_vaders_tabata/core/theme/app_text_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -6,12 +9,39 @@ class FirstAccessPage extends GetView<FirstAccessController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('FirtAccessPage')),
-      body: SafeArea(
-        child: TextButton(
-          child: Text("Criar Tabata"),
-          onPressed: controller.createTabata,
-        ),
+      backgroundColor: AppColors.defaultBackgroundColors,
+      body: Column(
+        children: [
+          SizedBox(
+            child: Image.asset(AssetConsts.FIRST_ACCESS_IMAGE),
+          ),
+          Column(
+            children: [
+              const SizedBox(height: 32),
+              AppTextTheme.Header(
+                  "Vamos começar o seu primeiro\nTreino Tabata?"),
+              const SizedBox(height: 8),
+              AppTextTheme.Subtitle(
+                  "No próximo passo você vai criar o Tabata ideal\npara o seu treino."),
+              const SizedBox(height: 48),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: GestureDetector(
+                  onTap: controller.createTabata,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xFF603CA2),
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(24)),
+                    width: double.maxFinite,
+                    height: 48,
+                    child: Center(child: AppTextTheme.Button("Criar Tabata")),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
---
+++ lib/core/theme/app_text_theme.dart
@@ -0,0 +1,23 @@
+import 'package:darth_vaders_tabata/core/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class AppTextTheme {
+  static final TextStyle _textStyle =
+      GoogleFonts.lato().copyWith(color: AppColors.defaultTextColors);
+
+  static Widget Normal(String value) =>
+      Text(value, style: _textStyle, textAlign: TextAlign.center);
+
+  static Widget Header(String value) => Text(value,
+      style: _textStyle.copyWith(fontWeight: FontWeight.w900, fontSize: 20),
+      textAlign: TextAlign.center);
+
+  static Widget Button(String value) => Text(value,
+      style: _textStyle.copyWith(fontWeight: FontWeight.w700, fontSize: 18),
+      textAlign: TextAlign.center);
+
+  static Widget Subtitle(String value) => Text(value,
+      style: _textStyle.copyWith(fontWeight: FontWeight.w700, fontSize: 14),
+      textAlign: TextAlign.center);
+}
