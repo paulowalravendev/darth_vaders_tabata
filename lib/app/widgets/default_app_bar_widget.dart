fba463e0b1e878bd208804dac87dd52989737f9d
victor_divino <victor_divino@mobills.com.br>
2022-06-04 20:51:43 -0300

    created default app bar

---
+++ lib/app/widgets/default_app_bar_widget.dart
@@ -0,0 +1,32 @@
+import 'package:darth_vaders_tabata/core/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class DefaultAppBarWidget extends StatelessWidget with PreferredSizeWidget {
+  @override
+  final Size preferredSize;
+  final Text? title;
+  final Color? color;
+  final bool showBottonBorder;
+
+  DefaultAppBarWidget({
+    this.title,
+    this.color,
+    this.showBottonBorder = true,
+    Key? key,
+  })  : preferredSize = const Size.fromHeight(50.0),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: color ?? Colors.transparent,
+      elevation: showBottonBorder ? null : 0,
+      centerTitle: true,
+      title: title,
+      leading: IconButton(
+        icon: const Icon(Icons.close, color: AppColors.defaultTextColors),
+        onPressed: () => Navigator.of(context).pop(),
+      ),
+    );
+  }
+}
