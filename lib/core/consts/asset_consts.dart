99a0ee715fcf7b36b6c3bfddb80f79af0bd479d9
Paulo Walraven <pmwc03@gmail.com>
2022-06-04 17:48:56 -0300

    create first access page

--- lib/core/consts/asset_consts.dart
+++ lib/core/consts/asset_consts.dart
@@ -1,3 +1,4 @@
 abstract class AssetConsts {
   static final SPLASH_LOTTIE = "assets/lottie/splash_lottie.json";
+  static final FIRST_ACCESS_IMAGE = "assets/images/first-access-background.png";
 }
--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors {
   static const defaultBackgroundColors = Color(0xFF17191C);
+  static const defaultTextColors = Color(0xFFFFFFFF);
 }
