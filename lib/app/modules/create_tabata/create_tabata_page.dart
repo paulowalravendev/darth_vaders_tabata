3906c8e8387460620603bd8577920c62e2ee73cc
victor_divino <victor_divino@mobills.com.br>
2022-06-04 20:51:43 -0300

    created default app bar

--- lib/app/modules/create_tabata/create_tabata_page.dart
+++ lib/app/modules/create_tabata/create_tabata_page.dart
@@ -1,14 +1,19 @@
-import 'package:darth_vaders_tabata/app/modules/create_tabata/create_tabata_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'package:darth_vaders_tabata/app/modules/create_tabata/create_tabata_controller.dart';
+import 'package:darth_vaders_tabata/app/widgets/default_app_bar_widget.dart';
+import 'package:darth_vaders_tabata/core/theme/app_colors.dart';
+
 class CreateTabataPage extends GetView<CreateTabataController> {
+  final String _title = "Configurações";
   const CreateTabataPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('CreateTabataPage')),
+      backgroundColor: AppColors.defaultBackgroundColors,
+      appBar: DefaultAppBarWidget(title: Text(_title)),
       body: SafeArea(
         child: TextButton(
           child: Text("Criar Tabata"),
