f803b84a5ab82e724c0d841d3c994fa7eaf7bde9
Paulo Walraven <pmwc03@gmail.com>
2022-06-05 07:14:32 -0300

    v1 tabata home

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,8 +1,25 @@
+import 'package:darth_vaders_tabata/app/data/models/tabata_model.dart';
+import 'package:darth_vaders_tabata/app/data/models/training_model.dart';
+import 'package:darth_vaders_tabata/app/data/services/tabata_service.dart';
+import 'package:darth_vaders_tabata/app/data/services/training_service.dart';
 import 'package:darth_vaders_tabata/app/routes/routes.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  void playerTabata() {
+  var selectedTab = 0.obs;
+
+  void changeTab(int index) {
+    selectedTab.value = index;
+  }
+
+  void toCreateTabata() {
+    Get.toNamed(Routes.CREATE_TABATA);
+  }
+
+  void toTabataPlay() {
+    Get.find<TrainingService>().createTraining(getTabata());
     Get.toNamed(Routes.TABATA_PLAYER);
   }
+
+  TabataModel getTabata() => Get.find<TabataService>().getTabata();
 }
